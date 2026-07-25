016d6dd93f30379ba98cdd361241ddc1919cb61d
Anshu <anshpoddar89@gmail.com>
2026-07-25 14:34:36 +0530

    O_n_Square

---
+++ O_n_square.py
@@ -0,0 +1,13 @@
+def find_duplicate(numbers):
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j and numbers[i]==numbers[j]:
+                return True
+    return False
+
+numbers=[10,22,35,22,48]
+result=find_duplicate(numbers)
+if result:
+    print('Found')
+else:
+    print('Not Found')
