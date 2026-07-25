16e9db30623a652baf3eba548edfcd0e4eba95af
Anshu <anshpoddar89@gmail.com>
2026-07-25 14:49:27 +0530

    Space&Time Complexity(Less Memory More time)

---
+++ Space&Timeway1.py
@@ -0,0 +1,14 @@
+#O(n^2)
+def has_duplicate_slow(numbers):
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i!=j and numbers[i]==numbers[j]:
+                return True
+    return False
+
+numbers=[10,20,30,40,50,10]
+result=has_duplicate_slow(numbers)
+if result:
+    print(f"Duplicate Found")
+else:
+    print('Not found')
