7005f18eaecbe4073e859f4904e0708ae44c6dc1
Anshu <anshpoddar89@gmail.com>
2026-08-01 16:09:56 +0530

    Collison in hashing

---
+++ Coll_in_hashing.py
@@ -0,0 +1,10 @@
+users={}
+
+users['ravi@gmail.com']='Ravi Sharma'
+users['ravi@gmail.com']='Ravi Sharma'
+
+print(hash('ravi@gmail.com'))
+print(hash('ravi@gmail.com'))
+
+#For example box 4 is taken. Python checks box 5, Box 5 is empty, other will go to box 5
+#Python remembers this internally 
